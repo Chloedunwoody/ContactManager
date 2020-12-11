ae48ff1ad3cd08dba921ca0fcc5df56c5cff8ed9
Tymofiy Bun <1931850@champlaincollege.qc.ca>
2020-12-11 14:11:20 -0500

    Last style update

--- ContactManager/AddPerson.xaml.cs
+++ ContactManager/AddPerson.xaml.cs
@@ -56,5 +56,34 @@ namespace ContactManager
 
             }
         }
+        private void PowerOff_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
     }
 }
--- ContactManager/UpadatePerson.xaml.cs
+++ ContactManager/UpadatePerson.xaml.cs
@@ -69,5 +69,35 @@ namespace ContactManager
             EmailBox.Text = person.Email;
             PhoneBox.Text = person.Phone;
         }
+
+        private void PowerOff_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
     }
 }
